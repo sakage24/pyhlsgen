5f7b01e02d17b4eae568dd67af8756ee85819196
kiwi <info@kiwi-bird.xyz>
2018-12-03 14:58:09 +0900

    windows用のテストと条件分けをしました

--- info/tests.py
+++ info/tests.py
@@ -6,7 +6,8 @@ import unittest
 
 class TestRun(unittest.TestCase):
     def setUp(self):
-        pass
+        if Source.PLATFORM.value == 'win32':
+            Source.SOURCE_FILE_DIRECTORY._value_ = 'd:\\AV'
 
     def tearDown(self):
         try:
@@ -15,16 +16,19 @@ class TestRun(unittest.TestCase):
             pass
 
     def test_get_media_list(self):
-        Source.SOURCE_FILE_DIRECTORY._value_ = 'd:\\AV'
         manager = Manager()
         self.assertTrue(manager.get_movie_list())
         for i in manager.get_movie_list():
             print(i)
 
     def test_makedir(self):
-        Source.DESTINATION_FILE_DIRECTORY._value_ = 'c:\\Users\\aaaok\\Desktop\\testCreated'
         manager = Manager()
-        self.assertTrue(manager.make_directory(Source.DESTINATION_FILE_DIRECTORY.value))
+        if Source.PLATFORM.value == 'win32':
+            Source.DESTINATION_FILE_DIRECTORY._value_ = 'c:\\Users\\aaaok\\Desktop\\testCreated'
+            self.assertTrue(manager.make_directory(Source.DESTINATION_FILE_DIRECTORY.value))
+            Source.DESTINATION_FILE_DIRECTORY._value_ = 'm3u8'
+        else:
+            self.assertTrue(manager.make_directory(Source.DESTINATION_FILE_DIRECTORY.value))
 
     def test_replace_character(self):
         manager = Manager()
@@ -33,6 +37,11 @@ class TestRun(unittest.TestCase):
         result = manager.get_replace_character(word)
         self.assertEqual(result, expected_word)
         print(result, expected_word)
+
+    def test_working_directory_path(self):
+        working_directory = os.path.join(Source.SOURCE_FILE_DIRECTORY.value, Source.DESTINATION_FILE_DIRECTORY.value)
+        print(working_directory)
+
 
 if __name__ == '__main__':
     unittest.main()
